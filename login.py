3c8c0496df31f90db7e638cbac55548177c26421
Monteiro Hanrry <hanrryravel159357@gmail.com>
2025-10-04 19:17:54 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,9 @@
+usuario_correto = "Carlos Gustavo"
+senha_correta = "ideação123"
+
+usuario = input("Digite seu usúario: ")
+senha = input("Digite sua senha: ")
+if usuario == usuario_correto and senha == senha_correta:
+	print('é isso você entrou agora cuidado com o que você faz')
+else:
+	print('tá pensando que eu sou louco, vai tentar de novo')
